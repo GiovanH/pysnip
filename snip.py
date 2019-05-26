b3785fc90a142034cc8aa89ab4bce78042a503a2
Gio <sethg@ipi.org>
2019-05-25 23:47:53 -0500

    Bytes to string helper

--- snip.py
+++ snip.py
@@ -54,6 +54,17 @@ def timer():
         print(time_taken)
 
 
+def bytes_to_string(bytes, units=['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB'], sep="", base=1024):
+    """ Returns a human readable string reprentation of bytes."""
+    # Adapted from a comment by "Mr. Me" on github.
+    if bytes < base:
+        return "{:0.2f}{}{}".format(bytes, sep, units[0])
+    else:
+        return bytes_to_string(bytes / base, units[1:], sep=sep)
+
+
+# User prompts
+
 # Streams
 
 
