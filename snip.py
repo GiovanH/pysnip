62f5e4dabd0de87583452cdae58b6910b7d2d046
Gio <sethg@ipi.org>
2019-05-25 23:48:11 -0500

    Stream teeing

--- snip.py
+++ snip.py
@@ -135,13 +135,13 @@ class DefaultFormatter(Formatter):
 
 
 @contextmanager
-def std_redirected(filename, errname=None):
+def std_redirected(filename, errname=None, tee=False):
     """Summary
-    
+
     Args:
         filename (TYPE): Description
         errname (None, optional): Description
-    
+
     Yields:
         TYPE: Description
     """
@@ -160,6 +160,10 @@ def std_redirected(filename, errname=None):
     sys.stdout = open(filename, 'w')
     sys.stderr = open(errname, 'w') if filename != errname else sys.stdout
 
+    if tee:
+        sys.stdout = stream_tee(sys.stdout, _stdout)
+        sys.stderr = stream_tee(sys.stderr, _stderr)
+
     try:
         yield None
     finally:
@@ -167,6 +171,30 @@ def std_redirected(filename, errname=None):
         sys.stderr.close()  # Safe to use even if stdout == stderr
         sys.stdout = _stdout
         sys.stderr = _stderr
+
+
+class stream_tee(object):
+    # Based on https://gist.github.com/327585 by Anand Kunal
+    def __init__(self, stream1, stream2):
+        self.stream1 = stream1
+        self.stream2 = stream2
+        self.__missing_method_name = None  # Hack!
+
+    def __getattribute__(self, name):
+        return object.__getattribute__(self, name)
+
+    def __getattr__(self, name):
+        self.__missing_method_name = name  # Could also be a property
+        return getattr(self, '__methodmissing__')
+
+    def __methodmissing__(self, *args, **kwargs):
+            # Emit method call to the log copy
+        callable2 = getattr(self.stream2, self.__missing_method_name)
+        callable2(*args, **kwargs)
+
+        # Emit method call to stdout (stream 1)
+        callable1 = getattr(self.stream1, self.__missing_method_name)
+        return callable1(*args, **kwargs)
 
 # String operations
 
