4e8c6c2c4b34e395de406ae30cc161c8cf7cf6d8
Seth Giovanetti <gio@utdallas.edu>
2019-06-06 21:37:46 -0500

    Lazy import

--- snip/__init__.py
+++ snip/__init__.py
@@ -6,7 +6,12 @@ Snip: A library of generic helper functions for utility programs.
 """
 
 from . import data, filesystem, flow, hash, stream, string 
-from . import jfileutil, tkit, pwidgets, loom
+from . import jfileutil, pwidgets, loom
 from . import prompt
-from . import image, audio
 from . import nest
+
+try:
+    from . import image, audio
+    from . import tkit
+except ModuleNotFoundError as e:
+    pass
