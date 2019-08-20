67bbc30e741cbd1e7fc72185fcdba6354887539e
Gio <sethg@ipi.org>
2019-08-19 21:56:10 -0500

    Renaming hash functions

--- snip/hash.py
+++ snip/hash.py
@@ -1,7 +1,7 @@
 # Hashing
 
 
-def md5(path):
+def md5file(path):
     """Returns the md5 hash of the file at (str) path.
 
     Args:
@@ -17,8 +17,22 @@ def md5(path):
         hasher.update(buf)
     return hasher.hexdigest()
 
+def md5data(data):
+    """Returns the md5 hash of some data
 
-def CRC32(filename):
+    Args:
+        data: Binary data
+
+    Returns:
+        str: Hex digest of md5 hash
+    """
+    from hashlib import md5
+    hasher = md5()
+    hasher.update(data)
+    return hasher.hexdigest()
+
+
+def CRC32file(filename):
     """Returns the CRC32 "hash" of the file at (str) path.
 
     Args:
@@ -31,4 +45,19 @@ def CRC32(filename):
     from binascii import crc32
     buf = open(filename, 'rb').read()
     buf = (crc32(buf) & 0xFFFFFFFF)
+    return "{:08X}".format(buf)
+
+
+def CRC32data(data):
+    """Returns the CRC32 "hash" of some data
+
+    Args:
+        data: Binary data
+
+    Returns:
+        str: Formated CRC32, as {:08X} formatted.
+
+    """
+    from binascii import crc32
+    buf = (crc32(data) & 0xFFFFFFFF)
     return "{:08X}".format(buf)
