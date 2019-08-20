ab00b154bb1d5091e65469ba239ad4eda428e910
Gio <sethg@ipi.org>
2019-08-19 21:56:17 -0500

    Extended image functionality

--- snip/image.py
+++ snip/image.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from PIL import ExifTags
 
 
 def framesInImage(im):  
@@ -12,3 +13,24 @@ def framesInImage(im):
             im.seek(frames + 1)
     except EOFError:
         return frames + 1
+
+
+def autoRotate(image):
+
+    orientationflags = [key for key in ExifTags.TAGS.keys() if ExifTags.TAGS[key] == 'Orientation']
+    try:
+        for orientation in orientationflags:
+            exif = dict(image._getexif().items())
+            if not exif.get(orientation):
+                continue
+
+            if exif[orientation] == 3:
+                return image.rotate(180, expand=True)
+            elif exif[orientation] == 6:
+                return image.rotate(270, expand=True)
+            elif exif[orientation] == 8:
+                return image.rotate(90, expand=True)
+    except (KeyError, AttributeError):
+        pass
+
+    return image
