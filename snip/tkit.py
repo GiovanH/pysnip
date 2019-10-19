5f4e0870b4b9da087fb8322a518c773c5535f2d9
Gio <sethg@ipi.org>
2019-10-19 00:12:28 -0500

    Vertical scrolling frame

--- snip/tkit.py
+++ snip/tkit.py
@@ -65,7 +65,6 @@ class MultiColumnListbox(tk.Frame):
         self.setup_widgets(headers, vscroll=vscroll, hscroll=hscroll)
         self.build_tree(headers, tabledata)
 
-
         if multiselect:
             self.tree.configure(selectmode=tk.NONE)
             self.bindSelectionAction("<Button-1>", self.tree.selection_toggle, useUID=True)
@@ -382,48 +381,33 @@ def makePhotoImage(filename, maxwidth, maxheight, ALWAYS_RESIZE=True):
     return r
 
 class VerticalScrolledFrame(tk.Frame):
-    """A pure Tkinter scrollable frame that actually works!
+    def __init__(self, root):
 
-    * Use the 'interior' attribute to place widgets inside the scrollable frame
-    * Construct and pack/place/grid normally
-    * This frame only allows vertical scrolling
-    
-    """
-    def __init__(self, parent, *args, **kw):
-        tk.Frame.__init__(self, parent, *args, **kw)            
+        tk.Frame.__init__(self, root)
+        self.canvas = tk.Canvas(root, borderwidth=0, background="#ffffff")
+        self.frame = tk.Frame(self.canvas, background="#ffffff")
+        self.vsb = tk.Scrollbar(root, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
 
-        # create a canvas object and a vertical scrollbar for scrolling it
-        vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
-        vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
-        canvas = tk.Canvas(self, bd=0, highlightthickness=0,
-                        yscrollcommand=vscrollbar.set)
-        canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
-        vscrollbar.config(command=canvas.yview)
+        self.vsb.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.create_window((4,4), window=self.frame, anchor="nw", 
+                                  tags="self.frame")
 
-        # reset the view
-        canvas.xview_moveto(0)
-        canvas.yview_moveto(0)
+        self.frame.bind("<Configure>", self.onFrameConfigure)
 
-        # create a frame inside the canvas which will be scrolled with it
-        self.interior = interior = tk.Frame(canvas)
-        interior_id = canvas.create_window(0, 0, window=interior,
-                                           anchor=tk.NW)
+    #     self.populate()
 
-        # track changes to the canvas and frame width and sync them,
-        # also updating the scrollbar
-        def _configure_interior(event):
-            # update the scrollbars to match the size of the inner frame
-            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
-            canvas.config(scrollregion="0 0 %s %s" % size)
-            if interior.winfo_reqwidth() != canvas.winfo_width():
-                # update the canvas's width to fit the inner frame
-                canvas.config(width=interior.winfo_reqwidth())
-        interior.bind('<Configure>', _configure_interior)
+    # def populate(self):
+    #     '''Put in some fake data'''
+    #     for row in range(100):
+    #         tk.Label(self.frame, text="%s" % row, width=3, borderwidth="1", 
+    #                  relief="solid").grid(row=row, column=0)
+    #         t="this is the second column for row %s" %row
+    #         tk.Label(self.frame, text=t).grid(row=row, column=1)
 
-        def _configure_canvas(event):
-            if interior.winfo_reqwidth() != canvas.winfo_width():
-                # update the inner frame's width to fill the canvas
-                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
-        canvas.bind('<Configure>', _configure_canvas)
+    def onFrameConfigure(self, event):
+        '''Reset the scroll region to encompass the inner frame'''
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
         return
