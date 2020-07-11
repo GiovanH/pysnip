eaafa007c9ae98c7f923968c51b11399950dc75d
Gio <sethg@ipi.org>
2020-07-10 20:23:26 -0500

    Tests for AIOSpool

--- tests/spool.py
+++ tests/spool.py
@@ -1,7 +1,9 @@
 import pytest
 import time
+import asyncio
+import sys
 
-from snip.loom import Spool
+from snip.loom import Spool, AIOSpool
 
 print('good day and welcome to tests')
 
@@ -33,13 +35,61 @@ class TestData(object):
         assert len(work) == max
 
     def test_print(self):
-        with Spool(8, "print") as spool:
+        with Spool(8, "print bar") as spool:
+            for i in range(0, 20):
+                spool.enqueue(dowork, ([], 1, f"Saying {i}"))
+
+        with Spool(8, "print", use_progbar=False) as spool:
             for i in range(0, 20):
                 spool.enqueue(dowork, ([], 1, f"Saying {i}"))
 
 
+class ATestData(object):
+
+    async def test_fast(self):
+        return
+
+    async def test_slow(self):
+        work = []
+
+        max = 40
+
+        async with AIOSpool(3, "slow") as spool:
+            for i in range(0, max):
+                spool.enqueue(adowork(work, i,))
+
+        assert len(work) == max
+
+    async def test_print(self):
+        async with AIOSpool(8, "print bar") as spool:
+            for i in range(0, 20):
+                spool.enqueue(adowork([], 1, f"Saying {i}"))
+
+        async with AIOSpool(8, "print", use_progbar=False) as spool:
+            for i in range(0, 20):
+                spool.enqueue(adowork([], 1, f"Saying {i}"))
+
+async def adowork(work, i, say=None):
+    time.sleep(.2)
+    work.append(i)
+    if say:
+        print(say)
+
+async def asynciomain():
+    orig_out, orig_out_err = sys.stdout, sys.stderr
+    await ATestData().test_fast()
+    await ATestData().test_print()
+    await ATestData().test_slow()
+    await AIOSpool(jobs=[adowork([], 1, f"Saying hi")]).finish()
+    assert orig_out is sys.stdout
+    assert orig_out_err is sys.stderr
+
 
 if __name__ == '__main__':
-    TestData().test_fast()
-    TestData().test_print()
-    TestData().test_slow()
+    orig_out, orig_out_err = sys.stdout, sys.stderr
+    # TestData().test_fast()
+    # TestData().test_print()
+    # TestData().test_slow()
+    assert orig_out is sys.stdout
+    assert orig_out_err is sys.stderr
+    asyncio.run(asynciomain())
